4496b72c4cfa6fcecff7283620cabc038217818c
TeddyGui <61049069+TeddyGui@users.noreply.github.com>
2020-05-20 17:22:08 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Credit from './Components/Credit'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+      <Credit />
+
+    </div>
+  );
+}
+
+export default App;
